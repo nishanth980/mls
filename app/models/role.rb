7978858dc76f9d63e83685c739b13282d1d1f224
nishanth980 <nishanth.mosam@gmail.com>
2014-07-09 18:41:35 +0530

    initial commit

---
+++ app/models/role.rb
@@ -0,0 +1,5 @@
+class Role < ActiveRecord::Base
+  attr_accessible :name, :user_id
+
+  belongs_to :user
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mls::Application.config.secret_token = 'd64cebaf9fae2c3ad764b38ae1d4cadfee57753f61b768b38fcf6eadf7b0806eeeb981088f32e949dca71ba1ff7059c765dd71b5ba7f0e79b0cc5e76366c1f87'

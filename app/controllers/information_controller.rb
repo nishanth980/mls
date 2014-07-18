4965608ca91aa42b1dce8c71ee934fd7a3769335
nishanth980 <nishanth.mosam@gmail.com>
2014-07-19 05:01:31 +0530

    final

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -129,7 +129,8 @@ end
 
 def export
  @v=Information.where(:exel_id => @@exel_id)
- @info = @v.order(:mls_id)
+ @val=@v.select("mls_id,mls_name,sale_type,listing_status,apn,address,suite,street,street_name,city,state,zip,sale_date,sale_price,concession_amount,owner,days_on_market,original_listing_price,original_listing_date,current_listing_price,current_listing_date,contact_date,financing_type,listing_status_date,public_remarks,agent_remarks")
+ @info = @val.order(:mls_id)
 
   respond_to do |format|
       format.html
--- app/models/information.rb
+++ app/models/information.rb
@@ -4,12 +4,17 @@ class Information < ActiveRecord::Base
 belongs_to :user
 
      def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << column_names
+    exclude_columns = ["id","user_id", "exel_id"]
+    @orignal_headers = column_names - exclude_columns
+
+      CSV.generate(options) do |csv|
+      csv << @orignal_headers
+       @arr = Information.select("mls_id,mls_name,sale_type,listing_status,apn,address,suite,street,street_name,city,state,zip,sale_date,sale_price,concession_amount,owner,days_on_market,original_listing_price,original_listing_date,current_listing_price,current_listing_date,contact_date,financing_type,listing_status_date,public_remarks,agent_remarks")
+      debugger
+      
       all.each do |information|
-        csv << information.attributes.values_at(*column_names)        
+        csv << information.attributes.values_at(*@orignal_headers )        
       end
     end
     end
-
 end
